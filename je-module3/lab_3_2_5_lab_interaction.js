8e3b88cc82052efa81042767c1a9f738c8710619
student <student@st05lab319.kin.tu.kielce.pl>
2026-04-30 09:24:46 +0200

    lab_3_2_5_lab_interaction

---
+++ je-module3/lab_3_2_5_lab_interaction.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')();
+let contacts = [{
+name: "Maxwell Wright",
+phone: "(0191) 719 6495",
+email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+}, {
+name: "Raja Villarreal",
+phone: "0866 398 2895",
+email: "posuere.vulputate@sed.com"
+}, {
+name: "Helen Richards",
+phone: "0800 1111",
+email: "libero@convallis.edu"
+}];
+
+// write your code here
+let newName = prompt("Enter the contact's name:");
+let newPhone = prompt("Enter the contact's phone number:");
+let newEmail =  prompt("Enter the contact's email:");
+
+contacts.push({
+    name: newName,
+    phone: newPhone,
+    email: newEmail
+});
+let last = contacts.length - 1;
+
+console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
